fb983a7ffa55479b7d59f75e2d71969f6db80ae7
Thiago Barbosa de Oliveira <36972689+ThiagoEXE@users.noreply.github.com>
2019-08-13 15:28:00 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,20 @@
+package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+ 
+         int idade = 23;
+         String nome = "Thiago";
+         String nomeDoMeuCachorro = "Ruby";
+         
+         idade = 25;
+         
+         System.out.println("Idade = " + idade);
+         System.out.println("Idade = " + nome);
+         System.out.println("Idade = " + nomeDoMeuCachorro);
+         
+   	
+	}
+
+}
